0ddd4ce118a68b47dbb00b1b009796fe8ef143a8
matteo <maty0698@gmail.com>
2022-01-31 11:28:28 +0100

    .

--- Mangaworld1/ecommerce/accounts/tests.py
+++ Mangaworld1/ecommerce/accounts/tests.py
@@ -44,7 +44,7 @@ class LogInTest(TestCase):
     '''
     def test_login(self):
         fake_credential = {'username':'ciao', 'password':'ciao'}
-        true_credential = {'username':'dumm', 'password':'nothing'}
+        true_credential = {'username':'dummy', 'password':'nothings'}
         t_cred = self.client.login(**true_credential)
         f_cred = self.client.login(**fake_credential)
 
